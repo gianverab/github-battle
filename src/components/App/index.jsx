6be4687bed87f746adb3edc0ffa838f8dd345062
Giancarlo Vera Burgos <Giancarlo Vera Burgos>
2017-03-16 00:06:09 -0300

    Updating template

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,10 +5,19 @@ import style from './app.css'
 import Main from '../Main'
 
 class App extends Component {
+	constructor () {
+		super()
+		this.state = {
+			user: {
+					name: 'Jane Doe',
+					email: 'janedoe@platzo.com'
+			}
+		}
+  }
 	render () {
 		return (
 			<div className={style.root}>
-				<Main />
+				<Main user={this.state.user}/>
 			</div>
 		)
 	}
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 class Main extends Component {
+  constructor (props) {
+		super(props)
+	}
 
 	render () {
 		return (
 			<div>
-        <h2>Hello React</h2>
+        <h2>Hello, I'm {this.props.user.name}</h2>
+        <h3>{this.props.user.email}</h3>
       </div>
 		)
 	}
