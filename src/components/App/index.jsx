d6567e9af3c1ebea9d4fe469cb32f4b6e5570848
Giancarlo Vera <gianverab@gmail.com>
2017-04-09 20:41:42 -0300

    Go back to basics

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,38 +7,11 @@ import Main from '../Main'
 import Profile from '../Profile'
 import Error404 from '../Error404'
 
-function handleClick (user, event) {
-  console.log('updated!')
-  return this.setState({
-    user: {
-      name: 'Giancarlo Vera',
-      email: 'gianverab@gmail.com',
-      picture: 'https://pbs.twimg.com/profile_images/1189582996/photo2-CV_400x400.jpg',
-      location: 'Buenos Aires',
-      url: 'classpoint.com'
-    }
-  })
-}
-
 class App extends Component {
   constructor () {
     super()
-    this.state = {
-      user: {
-        name: 'Damian Madray',
-        email: 'themadray@labdesign.com',
-        picture: 'https://pbs.twimg.com/profile_images/711367050174697473/qnkLKfgM_400x400.jpg',
-        location: 'San Francisco',
-        url: 'theglint.com'
-      }
-    }
-    this.handleClick = handleClick.bind(this, this.state.user)
   }
-  componentWillUpdate (nextProps, nextState) {
-    if (nextState.user !== this.state.user) {
-      this.handleClick = handleClick.bind(this, nextState.user)
-    }
-  }
+
   render () {
     return (
       <Router>
@@ -46,12 +19,12 @@ class App extends Component {
           <Header />
           <Match exactly pattern='/' render={() => {
             return (
-              <Main user={this.state.user} handleClick={this.handleClick} />
+              <Main />
             )
           }} />
           <Match pattern='/profile' render={() => {
             return (
-              <Profile user={this.state.user} />
+              <Profile />
             )
           }} />
           <Miss component={Error404} />
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-function Main ({ user, handleClick }) {
+function Main () {
   return (
     <div>
-      <h2>Hello, I'm {user.name}</h2>
-      <h3>{user.email}</h3>
-      <button onClick={handleClick}>Update!</button>
+      <h2>Home</h2>
       <Link to='/profile'>View profile</Link>
     </div>
   )
