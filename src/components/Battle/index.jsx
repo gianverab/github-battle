feeaa84f1f92af9f9f5eb22edcc3c3e4ff942da7
Giancarlo Vera <gianverab@gmail.com>
2017-04-27 00:05:15 -0300

    Building our battle component part2

--- src/components/Battle/index.jsx
+++ src/components/Battle/index.jsx
@@ -10,22 +10,50 @@ class PlayerInput extends Component {
     this.state = {
       username: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange (event) {
+    var value = event.target.value
+
+    this.setState({
+      username: value
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+
   render () {
     return (
       <div>
-        <form className={style.column} onSubmit={this.props.handleSubmit}>
+        <form className={style.column} onSubmit={this.handleSubmit}>
           <label className={style.heading} htmlFor='username'>
             {this.props.label}
           </label>
           <input
-            id={this.state.username}
+            id={this.props.id}
             placeholder='github username'
             type='text'
             autoComplete='off'
             value={this.state.username}
+            onChange={this.handleChange}
           />
-          <button type='submit'>Submit</button>
+          <button
+            className={style.button}
+            type='submit'
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
         </form>
       </div>
     )
@@ -75,11 +103,11 @@ class Battle extends Component {
           />}
 
         {!playerTwoName &&
-            <PlayerInput
-              id='playerTwo'
-              label='Player Two'
-              onSubmit={this.handleSubmit}
-            />}
+          <PlayerInput
+            id='playerTwo'
+            label='Player Two'
+            onSubmit={this.handleSubmit}
+          />}
       </div>
     )
   }
